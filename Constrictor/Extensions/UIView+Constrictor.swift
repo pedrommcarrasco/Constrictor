b4a5c8683c4b02729c54844a2095285fed5d06d1
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-05-21 21:29:13 +0100

    Revamped Constrictor to work with NSLayoutConstraints instead of anchors (easier time with priorities and relations)

--- Constrictor/Extensions/UIView+Constrictor.swift
+++ Constrictor/Extensions/UIView+Constrictor.swift
@@ -9,31 +9,61 @@
 import UIKit
 
 public extension UIView {
-
-    @discardableResult func constrict(_ type: ConstrictPosition,
-                                      to view: UIView? = nil,
-                                      spacing: CGFloat) -> UIView {
+    
+    @discardableResult func constrictToContainer(_ selfAttribute: NSLayoutAttribute,
+                                                 relation: NSLayoutRelation = .equal,
+                                                 attribute: NSLayoutAttribute = .notAnAttribute,
+                                                 constant: CGFloat = 0.0,
+                                                 multipler: CGFloat = 1.0,
+                                                 priority: UILayoutPriority = .required) -> UIView {
         
-        guard let superview = superview else { return self }
-
-        type.constrict(viewA: self, to: view ?? superview, spacing: spacing)
-
+        constrict(selfAttribute,
+                  relation: relation,
+                  to: superview,
+                  attribute: attribute,
+                  constant: constant,
+                  multipler: multipler,
+                  priority: priority)
+        
         return self
     }
-
-    @discardableResult func constrict(_ type: ConstrictSize,
+    
+    @discardableResult func constrict(_ selfAttribute: NSLayoutAttribute,
+                                      relation: NSLayoutRelation = .equal,
                                       to view: UIView? = nil,
-                                      size: CGFloat? = nil) -> UIView {
-
-        switch type {
-        case .width:
-            break
-        case .height:
-            break
+                                      attribute: NSLayoutAttribute = .notAnAttribute,
+                                      constant: CGFloat = 0.0,
+                                      multipler: CGFloat = 1.0,
+                                      priority: UILayoutPriority = .required) -> UIView {
+        
+        NSLayoutConstraint(item: self,
+                           attribute: selfAttribute,
+                           relatedBy: relation,
+                           toItem: view,
+                           attribute: attribute,
+                           multiplier: multipler,
+                           constant: constant).isActive = true
+        
+        return self
+    }
+    
+    @discardableResult func constrict(_ attributes: NSLayoutAttribute ...,
+                                      relation: NSLayoutRelation = .equal,
+                                      to view: UIView? = nil,
+                                      constant: CGFloat = 0.0,
+                                      multipler: CGFloat = 1.0,
+                                      priority: UILayoutPriority = .required) -> UIView {
+        
+        attributes.forEach {
+            NSLayoutConstraint(item: self,
+                               attribute: $0,
+                               relatedBy: relation,
+                               toItem: view,
+                               attribute: $0,
+                               multiplier: multipler,
+                               constant: constant).isActive = true
         }
-
+        
         return self
     }
 }
-
-
