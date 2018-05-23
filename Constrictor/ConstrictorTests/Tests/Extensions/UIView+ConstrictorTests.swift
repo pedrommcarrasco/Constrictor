5aed35cc3c00e9723a955c45ab4068db14b39277
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-05-23 12:02:20 +0100

    Attempt to fix TravisCI issues with unit testing.

---
+++ Constrictor/ConstrictorTests/Tests/Extensions/UIView+ConstrictorTests.swift
@@ -0,0 +1,26 @@
+//
+//  UIView+ConstrictorTests.swift
+//  ConstrictorTests
+//
+//  Created by Pedro Carrasco on 23/05/2018.
+//  Copyright © 2018 Pedro Carrasco. All rights reserved.
+//
+
+import XCTest
+
+class UIViewConstrictorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true)
+    }
+}
