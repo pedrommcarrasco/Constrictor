e25f26422e79b05e77ab369715df774438ea0642
Pedro Carrasco <pedrommcarrasco@gmail.com>
2018-05-27 20:05:24 +0100

    UIView+ConstrictorCenter's working with all iOS versions. Updated Example.

--- Constrictor/Constrictor/Extensions/UIView+ConstrictorCenter.swift
+++ Constrictor/Constrictor/Extensions/UIView+ConstrictorCenter.swift
@@ -5,76 +5,94 @@
 ////  Created by Pedro Carrasco on 21/05/2018.
 ////  Copyright © 2018 Pedro Carrasco. All rights reserved.
 ////
-//
-//import Foundation
-//
-//public extension UIView {
-//
-//    /**
-//     Centers the caller in its superview.
-//
-//     - parameters:
-//        - withinSafeArea: Boolean indicating if the constraint should be applied to the view's safeArea.
-//        - relation: Establish a relation between both attributes with NSLayoutRelation.
-//        - constant: CGFloat's value to add to the constraint.
-//        - multiplier: CGFloat's multiplier based on the values from both selfAttribute and attribute.
-//        - priority: UILayoutPriority that defines the constraint priority.
-//
-//     - returns:
-//     Discardable UIView to allow function's chaining.
-//     */
-//    @discardableResult func constrictCenterInContainer(withinSafeArea: Bool = true, relation: NSLayoutRelation = .equal,
-//                                                       constant: CGFloat = 0.0, multiplier: CGFloat = 1.0,
-//                                                       priority: UILayoutPriority = .required) -> UIView {
-//
-//        guard let superview = superview else { return self }
-//        
-//        constrictCenter(to: superview,
-//                        withinSafeArea: withinSafeArea,
-//                        relation: relation,
-//                        constant: constant,
-//                        multiplier: multiplier,
-//                        priority: priority)
-//        
-//        return self
-//    }
-//
-//    /**
-//     Centers the caller in the view sent by parameter.
-//
-//     - parameters:
-//        - view: UIView to match with the caller's .centerX and .centerY.
-//        - withinSafeArea: Boolean indicating if the constraint should be applied to the view's safeArea.
-//        - relation: Establish a relation between both attributes with NSLayoutRelation.
-//        - constant: CGFloat's value to add to the constraint.
-//        - multiplier: CGFloat's multiplier based on the values from both selfAttribute and attribute.
-//        - priority: UILayoutPriority that defines the constraint priority.
-//
-//     - returns:
-//     Discardable UIView to allow function's chaining.
-//     */
-//    @discardableResult func constrictCenter(to view: UIView, withinSafeArea: Bool = true,
-//                                            relation: NSLayoutRelation = .equal, constant: CGFloat = 0.0,
-//                                            multiplier: CGFloat = 1.0, priority: UILayoutPriority = .required) -> UIView {
-//
-//            constrict(.centerX,
-//                      relation: relation,
-//                      to: view,
-//                      withinSafeArea: withinSafeArea,
-//                      attribute: .centerX,
-//                      constant: constant,
-//                      multiplier: multiplier,
-//                      priority: priority)
-//
-//            constrict(.centerY,
-//                      relation: relation,
-//                      to: view,
-//                      withinSafeArea: withinSafeArea,
-//                      attribute: .centerY,
-//                      constant: constant,
-//                      multiplier: multiplier,
-//                      priority: priority)
-//        
-//        return self
-//    }
-//}
+
+import Foundation
+
+public extension UIView {
+    
+    /**
+     Description
+     
+     - parameters:
+     - viewController:
+     - relation:
+     - constant:
+     - constant:
+     - multiplier:
+     - priority:
+     - withinGuides:
+     
+     - returns:
+     Discardable UIView to allow function's chaining.
+     */
+    
+    @discardableResult
+    func constrictCenterInViewController(_ viewController: UIViewController, relation: NSLayoutRelation = .equal,
+                                        constant: CGFloat = 0.0, multiplier: CGFloat = 1.0,
+                                        priority: UILayoutPriority = .required, withinGuides: Bool = true) -> UIView {
+        
+        constrictCenter(relation, to: viewController, constant: constant,
+                       multiplier: multiplier, priority: priority, withinGuides: withinGuides)
+        
+        return self
+    }
+    
+    /**
+     Description
+     
+     - parameters:
+     - relation:
+     - constant:
+     - constant:
+     - multiplier:
+     - priority:
+     - withinGuides:
+     
+     - returns:
+     Discardable UIView to allow function's chaining.
+     */
+    
+    @discardableResult
+    func constrictCenterInSuperview(_ relation: NSLayoutRelation = .equal, constant: CGFloat = 0.0,
+                                   multiplier: CGFloat = 1.0, priority: UILayoutPriority = .required,
+                                   withinGuides: Bool = true) -> UIView {
+        
+        guard let superview = superview else { return self }
+        
+        constrictCenter(relation, to: superview, constant: constant,
+                       multiplier: multiplier, priority: priority, withinGuides: withinGuides)
+        
+        return self
+    }
+    
+    /**
+     Description
+     
+     - parameters:
+     - relation:
+     - item:
+     - constant:
+     - multiplier:
+     - priority:
+     - withinGuides:
+     
+     - returns:
+     Discardable UIView to allow function's chaining.
+     */
+    @discardableResult func constrictCenter(_ relation: NSLayoutRelation = .equal, to item: Constrictable,
+                                            constant: CGFloat = 0.0, multiplier: CGFloat = 1.0,
+                                            priority: UILayoutPriority, withinGuides: Bool = true) -> UIView {
+        
+        if withinGuides {
+            constrict(relation, to: item, attributes: .centerXGuide, .centerYGuide,
+                      constant: constant, multiplier: multiplier, priority: priority)
+            
+        } else {
+            constrict(relation, to: item, attributes: .centerX, .centerY,
+                      constant: constant, multiplier: multiplier, priority: priority)
+        }
+        
+        return self
+    }
+}
+
--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -24,31 +24,31 @@ class ViewController: UIViewController {
         view.addSubview(redView)
 
         // Constraints -> Same dimensions of redview's superview
-        redView.constrictEdgesToViewController(self)
-//
-//        // Or like it's done bellow
-//        /* redView.constrictToContainer(attributes: .top, .bottom, .leading, .trailing) */
-//
-//        // Or if you want an offset of 50 over all edges
-//        /*redView.constrictToContainer(attributes: .top, .bottom, .leading, .trailing, constant: 50.0) */
-//
-//        // ** Blue View **
-//        // Boilerplate
-//        blueView.backgroundColor = .blue
-//        redView.addSubview(blueView)
-//
-//        // Constraints -> 75 width, 100 height and centered in redview (superview)
-//        blueView.constrict(.width, constant: 75)
-//            .constrict(.height, constant: 100)
-//            .constrictCenterInContainer()
-//
-//        // ** Green View **
-//        // Boilerplate
-//        greenView.backgroundColor = .green
-//        redView.addSubview(greenView)
-//
-//        // Constraints -> Same width, height and centerY of blueView, greenView at the left of blueView with a spacing of 50
-//        greenView.constrict(attributes: .width, .height, .centerY, to: blueView)
-//            .constrict(.trailing, to: blueView, attribute: .leading, constant: 50)
+        redView.constrictEdgesToViewController(self, withinGuides: false)
+
+        // Or like it's done bellow
+        /* redView.constrictToContainer(attributes: .top, .bottom, .leading, .trailing) */
+
+        // Or if you want an offset of 50 over all edges
+        /*redView.constrictToContainer(attributes: .top, .bottom, .leading, .trailing, constant: 50.0) */
+
+        // ** Blue View **
+        // Boilerplate
+        blueView.backgroundColor = .blue
+        redView.addSubview(blueView)
+
+        // Constraints -> 75 width, 100 height and centered in redview (superview)
+        blueView.constrict(.width, constant: 75)
+            .constrict(.height, constant: 100)
+            .constrictCenterInViewController(self)
+
+        // ** Green View **
+        // Boilerplate
+        greenView.backgroundColor = .green
+        redView.addSubview(greenView)
+
+        // Constraints -> Same width, height and centerY of blueView, greenView at the left of blueView with a spacing of 8
+        greenView.constrict(to: blueView, attributes: .width, .height, .centerY)
+            .constrict(.trailing, to: blueView, attribute: .leading, constant: 8)
     }
 }
